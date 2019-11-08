1666a3ae54f084a79cf8f29471ab6f8c9f9cce18
MustafaMohamed001 <mustafa.mohamed@ufl.edu>
2019-11-08 02:10:43 -0500

    slight calendar formatting and date retrieval

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from '../../assets/logo.svg';
 import './Home.css';
 import Calendar from 'react-calendar';
 
-function Home() {
-    return (
-        <main>
-            <div clasName="row">
-                <div className="column1">
-                    <Calendar />
+class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    dateUpdate(date){
+        this.setState({date: date})
+        if(date){
+            console.log(date);
+        }
+    }
+
+    render(){
+        return (
+            <main>
+                <div className="row">
+                    <div className="column1">
+                        <div className="center">
+                            <Calendar 
+                                onChange={this.dateUpdate.bind(this)}
+                                value={this.state.date}
+                            />
+                        </div>
+                    </div>
+                    <div className="column2">
+                        <p className="center">Side Panel</p>
+                    </div>
                 </div>
-                <div className="column2">
-                    <p>Side Panel</p>
-                </div>
-            </div>
-        </main>
-    );
+            </main>
+        );
+    }
 }
 
 export default Home;
