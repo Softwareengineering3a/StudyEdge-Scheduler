063b1c4b5214bee9849453088101972800473eb1
Robert Rasmussen <jackras1234@ufl.edu>
2019-11-17 15:00:00 -0500

    changed sessionschema finishing emailcontroller

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,31 +1,35 @@
 var should = require('should'), 
 mongoose = require('mongoose'), 
-Reservation = require('../models/ReservationSchema'), 
+Session = require('../models/SessionSchema'), 
 Student = require('../models/StudentSchema'), 
 config = require('../config/config.js');
 
 mongoose.connect(config.db.uri, { useNewUrlParser: true });
 
-var myReservation, myStudent, sid, id;
+var mySession, myStudent, sid, id;
 
-myReservation =  {
-    date : "11/7/2019",
+mySession =  {
+    date : 'Sun Nov 17 2019 14:41:53 GMT-0500 (Eastern Standard Time)',
     time : 1300,
-    class : "Computer Organization",
+    class : "Software Engineering",
     tutor : "Miranda Overstreet",
-    location : "CSE352"
+    location : "CSE352",
+    slots : 420,
+    notes : "This class sucks",
+    title : "Testing",
+    students : ["Jessy", "James", "Carly", "Carl"]
 }
 
 myStudent = {
   UFID : 51037655,
   Name : "Robert Rasmussen",
   Email : "myEmail@nunya.com",
-  PhoneNumber : 3528703042,
+  PhoneNumber : 1112223333,
 }
 
 
-
-describe('Reservation and Student Schema Unit Tests', function() {
+  
+describe('Session and Student Schema Unit Tests', function() {
 
   it('saves student properly when required elements are provided', function(done){
       new Student({
@@ -40,15 +44,19 @@ describe('Reservation and Student Schema Unit Tests', function() {
       });
     });
 
-it('saves reservation properly when required elements are provided', function(done){
-    new Reservation({
-      date: myReservation.date, 
-      time: myReservation.time,
-      class: myReservation.class,
-      location: myReservation.location
-    }).save(function(err, myReservation){
+it('saves Session properly when required elements are provided', function(done){
+    new Session({
+      date: mySession.date, 
+      class: mySession.class,
+      tutor: mySession.tutor,
+      location: mySession.location,
+      slots: mySession.slots,
+      notes: mySession.notes,
+      title: mySession.title,
+      students: mySession.students
+    }).save(function(err, mySession){
       should.not.exist(err);
-      id = myReservation._id;
+      id = mySession._id;
       done();
     });
   });
