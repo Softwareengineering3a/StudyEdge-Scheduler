b3f749713d7456a439257b068089beaa3a33f597
Jada Dorman <jadadorman97@gmail.com>
2019-11-18 14:56:44 -0500

    Added delete button

--- client/src/components/DetailedSessionView/DetailedSessionView.js
+++ client/src/components/DetailedSessionView/DetailedSessionView.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 
 class DetailedSessionView extends Component {
@@ -11,9 +12,10 @@ class DetailedSessionView extends Component {
      myReservation = myReservation.filter(function(item) {
          return item.id === 1;
         }).map(function({date,time,course,title,location,slots}){
-        return {date,time,course,title,location,slots};
+
+            return {date,time,course,title,location,slots};
      });
-     console.log(myReservation.date)
+     console.log(myReservation)
      return(
         <Grid container  
             direction="column"
@@ -21,11 +23,12 @@ class DetailedSessionView extends Component {
             justify = "center"
             spacing={0}
             >
+                <Grid item>
+                    <Typography variant = 'h5'> Session Details </Typography>
+                </Grid>
             <Card>
-            <Grid item>
-                <Typography variant = 'h5'> Session Details </Typography>
-            </Grid>
-            <Grid item>
+            <Grid item
+            >
                     <Grid>Course</Grid>
                     <Grid>Title</Grid>
                     <Grid>Location</Grid>
@@ -33,7 +36,21 @@ class DetailedSessionView extends Component {
                     <Grid>Slots</Grid>
                     <Grid>Notes</Grid>
             </Grid>
-            </Card>       
+            </Card>    
+            <Grid container
+            direction="row"
+            spacing={0}> 
+            <Grid item>
+            <Button variant="contained">
+            Edit Session
+            </Button>
+            </Grid> 
+            <Grid item>
+            <Button variant="contained">
+            Delete
+            </Button>
+            </Grid> 
+            </Grid> 
         </Grid>
      );
   
