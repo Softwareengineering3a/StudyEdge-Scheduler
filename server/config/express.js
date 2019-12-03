c138e8b785e81d84a1d38078461431bd5c7cbbd8
Duharter <32145845+Duharter@users.noreply.github.com>
2019-12-03 07:49:31 -0500

    h5

--- server/config/express.js
+++ server/config/express.js
@@ -43,11 +43,11 @@ module.exports.init = () => {
 
     if (process.env.NODE_ENV === 'production') {
         // Serve any static files
-        app.use(express.static(path.join(__dirname, '../../client/build')));
+        app.use(express.static(path.join('../../client/build')));
 
         // Handle React routing, return all requests to React app
         app.get('*', function(req, res) {
-            res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
         });
     }
 
