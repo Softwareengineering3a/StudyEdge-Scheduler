f7563050fcf23eebfbe16da130fb207bc5b6add6
Duharter <duharter@ufl.edu>
2019-12-07 00:41:36 -0500

    heroku test 4

--- server/server.js
+++ server/server.js
@@ -1,7 +1,4 @@
 const express = require('./config/express.js')
  
-// Use env port or default
-const port = process.env.PORT || 5000;
-
 const app = express.init()
-app.listen(port, () => console.log(`Server now running on port ${port}!`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server now running on port 3000!`));
